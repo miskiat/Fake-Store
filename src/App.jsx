7cd7b52472a697ef95147103c85eac9596facbf2
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-08-28 02:07:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      Aisha Elenu Shockrusssssss
+    </div>
+  );
+}
+
+export default App;
