c2c94787eddfe026206ce56186c49f00c1289f7a
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-08-28 23:10:17 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>this is the Navbar</div>;
+  return (
+    <div className="navbar">
+      <img className="acme" src="/public/acme.svg" />
+
+      <h4>ACME STORE </h4>
+      <div>All Shirts Stickers</div>
+      <div>
+        <input placeholder="Search for Products..." />
+        <img className="search" src="/public/search.svg" />
+      </div>
+      <div>
+        <img className="cart" src="/public/cart.svg" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
