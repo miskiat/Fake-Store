25f117a0e3c425ef9f4c741ad4d3c1759f4fe3dc
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-08-31 23:17:00 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,17 @@ const Footer = () => {
   return (
     <div>
       <img className="acme" src="/public/acme.svg" />
+      <h4>ACME STORE</h4>
+      <div>
+        <p>Home</p>
+        <p>About</p>
+        <p>Terms & Condition</p>
+        <p>Shipping & Return Policy</p>
+
+        <p>Home</p>
+
+        <p>Home</p>
+      </div>
     </div>
   );
 };
