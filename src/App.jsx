83d570c7cabf826bf781fe169315bef5d4dc9abf
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-09-01 22:12:28 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,23 @@ function App() {
       <div className="side-options">
         <div>
           <p className="top">Collections</p>
-          <p>All</p>
-          <p>Men's clothing</p>
-          <p>Woman's clothing</p>
-          <p>Jewelery</p>
-          <p>Electronics</p>
+          <div className="">
+            <p>All</p>
+            <p>Men's clothing</p>
+            <p>Woman's clothing</p>
+            <p>Jewelery</p>
+            <p>Electronics</p>
+          </div>
         </div>
         <div>
           <p className="top">Sorts</p>
-          <p>Relevance</p>
-          <p>Trending</p>
-          <p>Latest arrivals</p>
-          <p>Price: Low to high</p>
-          <p>Price: High to low</p>
+          <div>
+            <p>Relevance</p>
+            <p>Trending</p>
+            <p>Latest arrivals</p>
+            <p>Price: Low to high</p>
+            <p>Price: High to low</p>
+          </div>
         </div>
       </div>
       <Footer />
