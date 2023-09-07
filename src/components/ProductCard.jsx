3328977b780910fbf238976594a5153a5060febd
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-09-07 22:28:08 +0100

    fixes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div>
-      <img className="bag" src="/public/81fPKd-2AYL._AC_SL1500_.jpg" />
+    <div className="bag">
+      <img src="/public/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <img src="/public/71li-ujtlUL._AC_UX679_.jpg" />
+      <img src="/public/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" />
     </div>
   );
 };
