82c3bbbc7557c152cb8aba78992101445df63201
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-09-08 22:15:33 +0100

    styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <div className="bag">
-      <img src="/public/81fPKd-2AYL._AC_SL1500_.jpg" />
-      <img src="/public/71li-ujtlUL._AC_UX679_.jpg" />
-      <img src="/public/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" />
+    <div>
+      <img className="bag" src="/public/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <img className="bag" src="/public/71li-ujtlUL._AC_UX679_.jpg" />
+      <img
+        className="bag"
+        src="/public/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+      />
     </div>
   );
 };
