a5ac74e71eebba80b2a5983ca8ad8734044d0707
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-10-26 22:21:17 +0100

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,13 @@ const Navbar = () => {
       <img className="acme" src="/public/acme.svg" />
 
       <h4>ACME STORE </h4>
+
       <div>All Shirts Stickers</div>
       <div className="search">
         <input placeholder="Search for Products..." />
         <img className="search-box" src="/public/search.svg" />
       </div>
+      <p className="flex-grow"></p>
       <div>
         <img className="cart" src="/public/cart.svg" />
       </div>
